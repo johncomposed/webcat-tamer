7b6525994a85dafc45b7fe2c818e43f65675b972
John Williams <johncomposed@gmail.com>
2015-04-06 13:34:21 -0400

    More Everything x2

--- app/tamerCtrl.js
+++ app/tamerCtrl.js
@@ -1,26 +1,101 @@
+wcTamer.controller('tamerCtrl', function tamerCtrl($scope, tamerStorage) {
 
-wcTamer.controller('tamerCtrl', function ($scope, tamerStorage) {
+  $scope.tamerStorage = tamerStorage;
 
-    $scope.tamerStorage = tamerStorage;
+  $scope.$watch('tamerStorage.data', function() {
+    $scope.hwList = $scope.tamerStorage.data;
+  });
 
-    $scope.$watch('tamerStorage.data', function() {
-        $scope.hwList = $scope.tamerStorage.data;
-    });
+  $scope.tamerStorage.findAll(function(data) {
+    $scope.hwList = data;
+    $scope.$apply();
+  });
 
-    $scope.tamerStorage.findAll(function(data){
-        $scope.hwList = data;
-        $scope.$apply();
-    });
+  $scope.removeAll = function() {
+    tamerStorage.removeAll();
+  };
 
-    $scope.removeAll = function() {
-        tamerStorage.removeAll();
+});
+
+wcTamer.controller('hwCtrl', function hwCtrl($scope, tamerStorage, $log, $routeParams, tamerUI) {
+  var hwID = $routeParams.id;
+  console.log('hwCtrl: ' + hwID);
+
+  // Yes, I know this is rubbish 
+  // TODO: fix
+  for (var i = 0; i < tamerStorage.data.length; i++) {
+    if (tamerStorage.data[i].time == hwID) {
+      console.log('hwID found');
+      $scope.currentTab = tamerStorage.data[i];
+      break; //eww
     }
-
-    $scope.toggleCompleted = function() {
-        tamerStorage.sync();
+    else {
+      console.log('Nothing found');
     }
+  }
 
-/*
+  $scope.tamerStorage = tamerStorage;
+
+  $scope.remove = function(hw) {
+    tamerStorage.remove(hw);
+  };
+
+  $scope.toggleLine = function() {
+    tamerUI.toggleLine();
+  };
+
+  $scope.print = function() {
+    tamerUI.print();
+  };
+});
+
+wcTamer.controller('dashCtrl', function dashCtrl($scope, tamerStorage, tamerUI) {
+  $scope.tamerStorage = tamerStorage;
+
+  $scope.$watch('tamerStorage.data', function() {
+    $scope.hwList = $scope.tamerStorage.data;
+  });
+
+  $scope.removeAll = function() {
+    tamerStorage.removeAll();
+  };
+
+  $scope.toggleLine = function() {
+    tamerUI.toggleLine();
+  };
+
+  $scope.print = function() {
+    tamerUI.print();
+  };
+
+
+});
+
+
+wcTamer.service('tamerUI', function() {
+
+  this.toggleLine = function() {
+    var lines = Array.prototype.slice.call(document.querySelectorAll(".lineCount, .lineCountHilight"));
+    var commentDivs = document.querySelectorAll('tr > td:not(:last-child) > div');
+    for (var i = 0; i < commentDivs.length; i++) {
+      lines.push(commentDivs[i].parentElement);
+    }
+    for (var i = 0; i < lines.length; i++) {
+      if (lines[i].style.display == 'none') {
+        lines[i].style.display = 'table-cell';
+      }
+      else {
+        lines[i].style.display = 'none';
+      }
+    }
+  }
+
+  this.print = function() {
+    window.print();
+  }
+
+
+  /*
   //myscopes
     $scope.currentTab = { title:'', content: ''};
 
@@ -31,52 +106,5 @@ wcTamer.controller('tamerCtrl', function ($scope, tamerStorage) {
     $scope.isActiveTab = function(tabUrl) {
         return tabUrl == $scope.currentTab;
     }*/
-});
-wcTamer.controller('hwCtrl', 
-    function hwCtrl($scope, tamerStorage, $log,$routeParams) {
-        var hwID  = $routeParams.id;
-        console.log('hwCtrl: ' + hwID);
-        for (var i=0;i < tamerStorage.data.length;i++) {
-            if(tamerStorage.data[i].time == hwID) {
-                console.log('hwID found');
-                $scope.currentTab = tamerStorage.data[i];
-                break; //eww
-            } else { console.log('Nothing found'); }
-        }
-    
-    $scope.tamerStorage = tamerStorage;
-
-    $scope.remove = function(hw) {
-        tamerStorage.remove(hw);
-    }
-
-    $scope.print = function() {
-        window.print();
-    }
-
-  $scope.toggleLine = function() {
-      var lines = Array.prototype.slice.call(document.querySelectorAll(".lineCount, .lineCountHilight"));
-      var commentDivs = document.querySelectorAll('tr > td:not(:last-child) > div');
-      for (var i = 0; i < commentDivs.length; i++) {
-        lines.push(commentDivs[i].parentElement);
-      }
-      for (var i = 0; i < lines.length; i++) {
-      if(lines[i].style.display == 'none') {
-         lines[i].style.display = 'table-cell';
-      } else {
-         lines[i].style.display = 'none';
-      }
-      }
-  }
-
-});
-
-wcTamer.controller('dashCtrl', function ($scope, tamerStorage) {
-
-    $scope.tamerStorage = tamerStorage;
-
-    $scope.$watch('tamerStorage.data', function() {
-        $scope.hwList = $scope.tamerStorage.data;
-    });
 
 });
--- inject.js
+++ inject.js
@@ -1,23 +1,31 @@
-// First just add the button
+// Only run if the back to summary button is found
 if (document.querySelector('[value="< Back to Summary"]')) {
+  var buttonOnSite = document.querySelector('[value="< Back to Summary"]');
+  var parentForm = buttonOnSite.parentElement;
+  var storage = chrome.storage.local;
+  var optionsUrl = chrome.extension.getURL('options.html');
 
-var buttonOnSite = document.querySelector('[value="< Back to Summary"]'),
-    parentForm = buttonOnSite.parentElement,
-    button = document.createElement("button"),
-    text = document.createTextNode("Save to Web-Cat Tamer");
-button.className += " tButton";
-button.type = 'button';
-button.appendChild(text);
-parentForm.appendChild(button);
-console.log("Button added!");
+  //console.log(storage);
+  var button, text;  
+  addButton();
+}
 
-//// Making it work
-var storage = chrome.storage.local; 
-var optionsUrl = chrome.extension.getURL('options.html');
 
-console.log(storage);
-button.addEventListener('click', saveCode);
 
+// Add the button
+function addButton() {
+  button = document.createElement("button");
+  text = document.createTextNode("Save to Web-Cat Tamer");
+  
+  button.className += " tButton";
+  button.type = 'button';
+  button.appendChild(text);
+  parentForm.appendChild(button);
+  button.addEventListener('click', saveCode);
+  
+  console.log("Button added!");
+
+}
 
 // define getKey()
 function getKey(key, callback) {
@@ -34,10 +42,11 @@ function getKey(key, callback) {
 function addBoxes(codeDataArr) {
   // Get all the code boxes on the page
   var boxes = document.querySelectorAll('div.expboxcontent'),
-      totalScore = document.querySelectorAll('table>tbody>tr>td')[4].innerHTML,
-      designScore = document.querySelectorAll('table>tbody>tr>td')[6].innerHTML,
-      styleScore = document.querySelectorAll('table>tbody>tr>td')[9].innerHTML,
-      testScore = document.querySelectorAll('table>tbody>tr>td')[13].innerHTML,
+      sBElement = document.querySelectorAll('table>tbody')[1],
+      designScore = sBElement.querySelectorAll('tr>td.R')[0].innerHTML,
+      styleScore = sBElement.querySelectorAll('tr>td.R')[1].innerHTML,
+      testScore = sBElement.querySelectorAll('tr>td.R')[2].innerHTML,
+      totalScore = sBElement.querySelectorAll('tr>td.R')[3].innerHTML,
       codeTitles = document.querySelectorAll('h2.collapsible');
 
   for (i = 0, l = boxes.length; i < l; i++) {
@@ -64,10 +73,10 @@ function addBoxes(codeDataArr) {
 }
 
 function message(msg) {
-  var message = document.querySelector('.message');
-  message.innerText = msg;
+  var m = document.querySelector('.message');
+  m.innerText = msg;
   setTimeout(function() {
-    message.innerText = '';
+    m.innerText = '';
   }, 3000);
 }
 
@@ -78,6 +87,7 @@ function goToOptions() {
 
 
 function saveCode() {
+  
   var codeDataArr;
   getKey('codeData', addBoxes);
   button.textContent = 'Go to Web-Cat Tamer';
@@ -86,4 +96,4 @@ function saveCode() {
   //button.innerHTML = ' <a target="_blank" href="' + optionsUrl + '">options page</a> ';
 
 }
-}
+
